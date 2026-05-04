1a4eef9db103017a05acf4509d69d386a34f76f5
YahyaYusufShaikh <yahyayusuf@rediffmail.com>
2026-05-04 14:24:01 +0530

    array sorted

--- 7_array_problems/array_sorted.cpp
+++ 7_array_problems/array_sorted.cpp
@@ -1,7 +1,25 @@
 #include<bits/stdc++.h>
 using namespace std;
 
+//array sorted or not
+bool isSorted(vector<int> v, int n){
+    for(int i = 1; i<n; i++){
+        if(v[i]>v[i-1]){
+
+        }else{
+            return false;
+        }
+    }
+   
+    return true;
+}
+
 int main(){
 
+    vector<int> arr = {3, 2, 4, 5, 6};
+    vector<int> arr1 = {1,2,3,4,5};
+
+    cout<<isSorted(arr, arr.size())<<endl;
+    cout<<isSorted(arr1, arr1.size())<<endl;
     return 0;
 }
