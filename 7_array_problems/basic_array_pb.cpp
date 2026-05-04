4bc194271e2ee312e9112a8828fed9d0e83a0dd0
YahyaYusufShaikh <yahyayusuf@rediffmail.com>
2026-05-04 13:04:08 +0530

    Largest element

--- 7_array_problems/basic_array_pb.cpp
+++ 7_array_problems/basic_array_pb.cpp
@@ -1,9 +1,19 @@
 #include<bits/stdc++.h>
-using namespace std:
+using namespace std;
 
 //finding largest element in array
+int largetst(vector<int> &arr, int n){
+    int max = arr[0];
+    for(int i = 0; i<n; i++){
+        if(arr[i]>max) max = arr[i];
+    }
+    return max;
+}
 
 int main(){
+
+    vector<int> arr = {3, 4, 7, 12, 9};
+    cout<<largetst(arr, arr.size());
 
     return 0;
 }
