e6b0f1e0aa6d69c36eaebb6505baa46fbe110eb8
YahyaYusufShaikh <yahyayusuf@rediffmail.com>
2026-05-04 13:14:13 +0530

    second largest element

--- 7_array_problems/basic_array_pb.cpp
+++ 7_array_problems/basic_array_pb.cpp
@@ -10,10 +10,20 @@ int largetst(vector<int> &arr, int n){
     return max;
 }
 
-int main(){
+//second largest
+int secondLargest(vector<int> &arr, int n){
+    int flargest = largetst(arr, n);
+    int slargest = -1;
+    for(int i = 0; i<n; i++){
+        if(arr[i]<flargest && arr[i]>slargest) slargest = arr[i];
+    }
+    return slargest;
+}
 
+int main(){
     vector<int> arr = {3, 4, 7, 12, 9};
     cout<<largetst(arr, arr.size());
-
+    cout<<endl;
+    cout<<secondLargest(arr, arr.size());
     return 0;
 }
