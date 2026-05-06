7930f8b2451b70f8dc55d6fd017864beb9446568
YahyaYusufShaikh <yahyayusuf@rediffmail.com>
2026-05-06 17:29:24 +0530

    Moves Zeros

---
+++ 7_array_problems/zeros_to_end.cpp
@@ -0,0 +1,31 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+
+void moveZeros(vector<int> &arr, int n){
+    int j = -1;
+    for(int i =0; i<n; i++){
+        if(arr[i] == 0){
+            j = i;
+            break;
+        }
+    }
+    if(j == -1) return ;
+    for(int i = j+1; i<n; i++){
+        if(arr[i]!=0){
+            swap(arr[j], arr[i]);
+            j++;
+        }
+    }
+}
+int main(){
+    
+    vector<int> arr = {1,0,2,3,2,0,0,4,5,0,1};
+    moveZeros(arr, arr.size());
+
+    for(int i = 0; i<arr.size(); i++){
+        cout<<arr[i]<<" ";
+    }
+
+    return 0;
+}
