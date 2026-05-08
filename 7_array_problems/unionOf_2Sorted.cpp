593c8332e52e5d0e090fea5a01ec6191c5fac049
YahyaYusufShaikh <yahyayusuf@rediffmail.com>
2026-05-08 10:32:45 +0530

    Union Of Two arrays

---
+++ 7_array_problems/unionOf_2Sorted.cpp
@@ -0,0 +1,30 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+
+vector<int> Union(vector<> arr1, vector<> arr2){
+    int n1 = arr1.size();
+    int n2 = arr2.size();
+
+    set<int> st;
+    for(int i=0; i<n1; i++){
+        st.insert(arr1[i]);
+    }
+    for(int i=0; i<n2; i++){
+        st.insert(arr2[i]);
+    }
+    vector<int> v;
+    for(auto it: st){
+        v.push_back(it);
+    }
+}
+
+int main(){
+    vector<int> a = {1,2,4,5,7,9};
+    vector<int> b = {2,3,4,7,10,11};
+
+    Union(a, b);
+
+
+    return 0;
+}
