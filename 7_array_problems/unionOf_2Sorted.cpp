e71f44dbb27413025e1d505e8be021fab9dd17d4
YahyaYusufShaikh <yahyayusuf@rediffmail.com>
2026-05-08 12:54:00 +0530

    Optimal Solution

--- 7_array_problems/unionOf_2Sorted.cpp
+++ 7_array_problems/unionOf_2Sorted.cpp
@@ -17,7 +17,40 @@ vector<int> Union(vector<int> arr1, vector<int> arr2){
     for(auto it: st){
         v.push_back(it);
     }
+
+    return v;
 }
+
+vector<int> optimal(vector<int> a, vector<int> b){
+    int n1 = a.size();
+    int n2 = b.size();
+    int i = 0;
+    int j = 0;
+    vector<int> temp;
+    while(i<n1 && j<n2)
+    {
+        if(a[i]<=b[j]){
+                if(temp.size() == 0 || temp.back() != a[i]){
+                    temp.push_back(a[i]);
+                }
+                i++;
+        }
+        else{
+                if(temp.size() == 0 || temp.back() != b[j]){
+                    temp.push_back(b[j]);
+                }
+                j++;
+        }
+    }
+    while(j<n1){
+        if(temp.size() == 0 || temp.back() != b[j]){
+            temp.push_back(b[j]);
+        }
+        j++;
+    } 
+    return temp;  
+}
+
 
 int main(){
     vector<int> a = {1,2,4,5,7,9};
@@ -26,6 +59,16 @@ int main(){
     vector <int> v = Union(a, b);
 
     cout<<v.size()<<endl;
+
+    for(auto it: v){
+        cout<<it<<" ";
+    }
+    cout<<endl;
+
+    vector<int> v1 = optimal(a, b);
+    for(auto it: v1){
+        cout<<it<<" ";
+    }
 
     return 0;
 }
