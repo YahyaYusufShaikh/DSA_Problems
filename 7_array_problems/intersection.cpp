bcdcfe731b2e1509257e09c2ad628c6e68430bbe
YahyaYusufShaikh <yahyayusuf@rediffmail.com>
2026-05-08 16:07:51 +0530

    intersection

---
+++ 7_array_problems/intersection.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+vector<int> intersection(vector<int> a, vector<int> b){
+    n1 = a.size();
+    n2 = b.size();
+    
+}
+
+int main(){
+
+
+
+    return 0;
+}
