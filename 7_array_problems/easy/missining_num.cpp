e96c62e703259f0e70c7655db04557a67b91a1b3
YahyaYusufShaikh <yahyayusuf@rediffmail.com>
2026-07-12 11:53:32 +0530

    better solution

---
+++ 7_array_problems/easy/missining_num.cpp
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+
+    int n = 5;
+    int num[5] = {1,2,4,5};
+    int arr[n+1] = {0};
+    for(int i = 0; i<n; i++){
+        arr[num[i]] = 1;
+    }
+    for(int i =1; i<=n; i++){
+        if(arr[i]==0){
+            cout<<i<<" ";
+        }
+    }
+    return 0;
+}
