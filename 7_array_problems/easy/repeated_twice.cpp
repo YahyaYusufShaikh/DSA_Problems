383d87ef000036f7403f8888663f06904f63f9ca
YahyaYusufShaikh <yahyayusuf@rediffmail.com>
2026-07-12 17:40:43 +0530

    single element form repeeated twice

---
+++ 7_array_problems/easy/repeated_twice.cpp
@@ -0,0 +1,17 @@
+#include <stdc/stdc++.h>
+using namespace std;
+
+int getSingle(vector<int> arr){
+    int XOR = 0;
+    for(int i = 0; i<arr.size(); i++){
+        XOR = XOR^arr[i];
+        cout<<XOR<<" "<<endl;
+    }
+    return XOR;
+}
+int main(){
+
+    vector<int> arr = {1,1,2,2,3,4,4};
+
+    return 0;
+}
