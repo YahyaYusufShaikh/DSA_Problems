90aed55f3b23cf9df430518457a7ec77e6a08d81
YahyaYusufShaikh <yahyayusuf@rediffmail.com>
2026-07-14 00:00:36 +0530

    optimal solution

--- 7_array_problems/easy/longSub_array_k.cpp
+++ 7_array_problems/easy/longSub_array_k.cpp
@@ -2,6 +2,11 @@
 using namespace std;
 
 //better and optimal solution
+int longestSubArrayWithSumK(vector<int> a, long long K){
+    
+}
+
+
 int main(){
 
     vector<int> arr {1,2,0,0,3,3,1,1,1,1,4,5,3,6,4,2};
