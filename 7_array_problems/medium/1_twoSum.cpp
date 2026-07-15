6c8f16271902f7f1bc25c81120c3839e6629cb79
YahyaYusufShaikh <yahyayusuf@rediffmail.com>
2026-07-15 20:53:20 +0530

    better solution

--- 7_array_problems/medium/1_twoSum.cpp
+++ 7_array_problems/medium/1_twoSum.cpp
@@ -1,24 +1,24 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-int twoSum(vector<int>& nums, int target){
+string twoSum(vector<int>& nums, int target){
     map<int, int> mpp;
     for(int i = 0; i<nums.size(); i++){
         int a = nums[i];
         int more = target - a;
         if(mpp.find(more) != mpp.end()){
-            return {mpp[more], i};
+            return "Yes";
         }
+        mpp[a] = i;
     }
 }
 
 int main(){
     vector<int> v = {4, 1, 2, 3, 1};
 
-    vector<int> v1 = twoSum(v, 5);
+    cout<<twoSum(v, 5);
 
-    for(auto it: v1){
-        cout<<it<<" ";
-    }
+    
+    
     return 0;
 }
