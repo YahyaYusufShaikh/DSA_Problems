a9c64b47a73405bcc963503d8449be79ac96f6e2
YahyaYusufShaikh <yahyayusuf@rediffmail.com>
2026-07-24 12:23:00 +0530

    better solution

--- 7_array_problems/medium/3_majority.cpp
+++ 7_array_problems/medium/3_majority.cpp
@@ -1,5 +1,18 @@
 #include <bits/stdc++.h>
 using namespace std;
+//better solution
+int majority(vector<int>v){
+    map<int, int> mpp;
+    for(int i = 0; i<v.size(); i++){
+        mpp[v[i]]++;
+    }
+    for(auto it: mpp){
+        if(it.second > v.size()/2){
+            return it.first;
+        }
+    }
+    return -1;
+}
 
 int main(){
 
