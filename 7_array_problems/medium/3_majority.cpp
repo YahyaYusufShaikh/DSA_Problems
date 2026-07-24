7977f0c10249af09a8c381e08a8ccfab6bc3b748
YahyaYusufShaikh <yahyayusuf@rediffmail.com>
2026-07-24 17:47:16 +0530

    Optimal Solution

--- 7_array_problems/medium/3_majority.cpp
+++ 7_array_problems/medium/3_majority.cpp
@@ -15,10 +15,32 @@ int majority(vector<int>v){
 }
 
 //moore's voting algorithm
+int majorityEl(vector<int>v){
+    int el, count = 0;
+    for(int i = 0; i<v.size(); i++){
+        if(count == 0){
+            el = v[i];
+            count = 1;
+        }else if(v[i] == el){
+            count++;
+        }else{
+            count--;
+        }
+    }
+    int cnt1 = 0;
+    for(int i = 0; i<v.size(); i++){
+        if(v[i] == el){
+            cnt1++;
+        }
+        if(cnt1 > (v.size()/2)){
+            return el;
+        }
+    }
+  }
 int main(){
 
     vector<int>v = {2,2,1,3,1,1,3,1,1};
     cout<<majority(v);
-
+    cout<<majorityEl(v);
     return 0;
 }
