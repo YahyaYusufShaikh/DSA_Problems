ac60fa8797fdd973fc305f4c8f77240fc6fc2c51
YahyaYusufShaikh <yahyayusuf@rediffmail.com>
2026-08-04 11:40:28 +0530

    Kadane's algorithm

--- 7_array_problems/medium/4_kadane's_algo.cpp
+++ 7_array_problems/medium/4_kadane's_algo.cpp
@@ -1,10 +1,20 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+long long maxSubArray(vector<int> &nums){
+    long long sum = 0, maxi = LONG_MIN;
+    for(i = 0; i<nums.size(); i++){
+        sum += nums[i];
+        if(sum > maxi) maxi = sum;
+        if(sum < 0) sum = 0;
+    }
+    return maxi;
+}
 
 int main(){
     
     vector<int>v = {2,2,1,3,1,1,3,1,1};
+
 
     cout<<"Started \n\n";
     return 0;
