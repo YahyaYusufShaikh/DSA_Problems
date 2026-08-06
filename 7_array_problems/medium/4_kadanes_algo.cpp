066151e51d1203b15599663a6de9329ff355670a
YahyaYusufShaikh <yahyayusuf@rediffmail.com>
2026-08-06 12:42:08 +0530

    Sub Array

--- 7_array_problems/medium/4_kadanes_algo.cpp
+++ 7_array_problems/medium/4_kadanes_algo.cpp
@@ -3,11 +3,27 @@ using namespace std;
 
 long long maxSubArray(vector<int> &nums){
     long long sum = 0, maxi = LONG_MIN;
+    int start, ansStart = -1, ansEnd = -1;
     for(int i = 0; i<nums.size(); i++){
+        if(start == 0) start = i;
         sum += nums[i];
-        if(sum > maxi) maxi = sum;
-        if(sum < 0) sum = 0;
+        if(sum > maxi){
+            maxi = sum; 
+            ansStart = start;
+            ansEnd = i;
+        }
+        if(sum < 0) {
+            sum = 0;
+            start = i+1;
+        };
+
     }
+
+        cout<<"Sub Array "<<endl;
+        for(int j = ansStart; j<=ansEnd; j++){
+            cout<<nums[j]<<" ";
+        }
+        cout<<endl;
     return maxi;
 }
 
@@ -16,8 +32,5 @@ int main(){
     
     vector<int>v = {-2,1,-3,4,-1, -2, 1, 5, -3};
     cout<<maxSubArray(v);
-
-
-    cout<<"Started \n\n";
     return 0;
 }
