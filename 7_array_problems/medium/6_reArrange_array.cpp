038426263e0727129f10037f098005ea3a381603
YahyaYusufShaikh <yahyayusuf@rediffmail.com>
2026-09-20 19:39:17 +0530

    re arrange array element

--- 7_array_problems/medium/6_reArrange_array.cpp
+++ 7_array_problems/medium/6_reArrange_array.cpp
@@ -2,6 +2,24 @@
 using namespace std;
 
 
+    vector<int> rearrangeArray(vector<int>& nums) {
+        vector<int> pos, neg;
+        int n = nums.size();
+        for(int i = 0; i < n; i++){
+            if(nums[i]>0){
+                pos.push_back(nums[i]);
+            }else{
+                neg.push_back(nums[i]);
+            }
+        }
+        if(pos.size() > neg.size()){
+
+        }else{
+            
+        }
+    }
+
+
 int main(){
 
 
